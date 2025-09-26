97efe2998ef134fd42d490b39d1c4cd1e7e2e63a
Gossard <jag.f.rosa@gmail.com>
2025-09-26 17:41:52 +0200

    WIP structures

--- includes/structure.h
+++ includes/structure.h
@@ -6,7 +6,7 @@
 /*   By: jgossard <jgossard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 18:14:27 by yukravch          #+#    #+#             */
-/*   Updated: 2025/09/25 15:47:18 by jgossard         ###   ########.fr       */
+/*   Updated: 2025/09/26 17:18:32 by jgossard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,13 @@ typedef enum e_surface_type {
 }	t_surface_type;
 
 typedef enum e_button {
-	W,
-	A,
-	S,
-	D,
-	LEFT,
-	RIGHT,
+	BUT_W,
+	BUT_A,
+	BUT_S,
+	BUT_D,
+	BUT_LEFT,
+	BUT_RIGHT,
 }	t_button;
-
-typedef struct s_texture {
-	t_direction	direction;
-	char		*path;
-}	t_texture;
 
 typedef struct s_surface_color {
 	t_surface_type	surface_type;
@@ -51,16 +46,38 @@ typedef struct s_point {
 	int	color; //TODO utile?
 }	t_point;
 
+/* ########  TEXTURE STRUCT PART  ######## */
+
+typedef struct s_texture_image
+{
+	void	*img;
+	char	*data;      // pixel buffer (int per pixel)
+	int		width;
+	int		height;
+	int		bpp;
+	int		line_len;
+	int		endian;
+}	t_texture_image;
+
+typedef struct s_texture
+{
+	t_direction			direction;
+	char				*path;
+	t_texture_image		image;
+}	t_texture;
+
+/* ########  MLX STRUCT PART  ######## */
+
 typedef struct s_mlx {
 	void	*ptr;
 	void	*win;
-	void	*img;
+	// void	*img; // move to its own structure
 }	t_mlx;
 
 /* ########  MAP STRUCT PART  ######## */
 
 typedef struct s_map {
-	t_point	**grid;
+	t_point	**grid; // grid[Y][x]
 	int		map_width;
 	int		map_height;
 }	t_map;
@@ -81,10 +98,19 @@ typedef struct s_player {
 	double	rotate_speed;
 }	t_player;
 
-/* ########  RENDERING + RAYCASTING STRUCT PART  ######## */
+/* ########  IMAGE + RENDERING + RAYCASTING STRUCT PART  ######## */
 
-typedef struct s_raycasting
+typedef struct s_image
 {
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		line_len;
+	int		endian;
+}	t_image;
+
+
+typedef struct s_raycasting {
 	// Ray direction linked to the player position on the map
 	double	ray_dir_x;
 	double	ray_dir_y;
@@ -112,37 +138,43 @@ typedef struct s_raycasting
 	int		side_hit;
 }	t_raycasting;
 
-typedef struct s_draw
-{
+typedef struct s_draw {
 	// height of line to draw
-	int line_height;
+	int		line_height;
 	// lowest pixel of wall
-	int draw_start;
+	int		draw_start;
 	// highest pixel of wall
-	int draw_end;
+	int		draw_end;
 	// x coordinate of texture
-	int tex_x;
+	int		tex_x;
 	// starting texture position
-	double tex_pos;
+	double	tex_pos;
 	// how much to increase tex_pos per screen pixel
-	double step;
+	double	step;
 }	t_draw;
 
 
 /* ########  MAIN STRUCT PART  ######## */
 typedef struct s_cub {
+	/* ########  PARSING / MAP DATA  ######## */
 	t_map			*map;
 	t_texture		*texture;
 	t_surface_color	*surface_color;
-
-	// t_player		*player;
-
+	/* ########  PLAYER DATA  ######## */
+	t_player		*player;
+	/* ########  MLX CONTEXT  ######## */
 	t_mlx			*mlx;
-// parsing:
+	/* ########  FRAME BUFFER (off-screen image) ######## */
+	t_image			frame;
+	/* ########  WINDOW / SCREEN INFO ######## */
+	int				screen_width;
+	int				screen_height;
+	/* ########  PARSING HELPERS ######## */
 	int				fd;
 	bool			map_flag;
 	char			*line;
 	char			**elements_of_line;
 }	t_cub;
+
 
 #endif
