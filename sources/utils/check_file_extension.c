f1ac85dd1438ba4b2aa5a1468441ae3507b68d1c
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2025-09-29 13:20:50 +0200

    fix: check extension for file path
    Exemple:
    file name like this ".cub" or ".xpm"
    
    before the code was checking
    if (len of file name < len of extension + 1) => 4 < 5
    	ERROR
    
    but I didn't test the case like this:
    path_to_file/.cub -> so in this case the file name is invalid as well
    BUT len of the file_name is not 4 but 17(the whole line with path)
    
    Now if there is any '/' in file_name - before checking the extension of the file
    I cut the file_name so It only has ".cub"

--- sources/utils/check_file_extension.c
+++ sources/utils/check_file_extension.c
@@ -6,17 +6,32 @@
 /*   By: yukravch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 12:50:44 by yukravch          #+#    #+#             */
-/*   Updated: 2025/09/26 13:36:42 by yukravch         ###   ########.fr       */
+/*   Updated: 2025/09/29 13:15:46 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+char	*ft_cut_file_name_from_path(char *path_to_file)
+{
+	char	*result;
+
+	result = ft_strrchr(path_to_file, '/');
+	if (result && result[1])
+		return (&result[1]);
+	else
+		return (NULL);
+}
 
 bool	ft_check_file_extension(char *file_name, char *extension)
 {
 	int	len_of_file_name;
 	int	len_of_extension;
 
+	if (ft_strchr(file_name, '/'))
+		file_name = ft_cut_file_name_from_path(file_name);
+	if (file_name == NULL)
+		return (false);
 	len_of_file_name = ft_strlen(file_name);
 	len_of_extension = ft_strlen(extension);
 	if (len_of_file_name < len_of_extension + 1)
