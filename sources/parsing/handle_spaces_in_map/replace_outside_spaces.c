e6c6179b1dcaac17c1773d33da392fca510fc3c5
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2025-10-07 15:07:10 +0200

    feat: replace outside SPACE on top and bottom with OUTSIDE_SPACE

--- sources/parsing/handle_spaces_in_map/replace_outside_spaces.c
+++ sources/parsing/handle_spaces_in_map/replace_outside_spaces.c
@@ -3,37 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   replace_outside_spaces.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yukravch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: yukravch <yukravch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 12:21:53 by yukravch          #+#    #+#             */
-/*   Updated: 2025/10/07 14:30:55 by yukravch         ###   ########.fr       */
+/*   Updated: 2025/10/07 15:06:29 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static void	ft_replace_first_line(t_cub *cub)
+static void	ft_replace_first_and_last_line(t_cub *cub, int line, int i, bool first)
 {
-	int	i;
-	int	line;
-
-	if (!cub)
+	if (!cub || !cub->map.grid || !cub->map.grid[line]
+			|| !cub->map.grid[line][i])
 		return ;
-	i = 0;
-	line = 0;
-	while (cub->map.grid && cub->map.grid[line] && cub->map.grid[line][i]
-			&& cub->map.grid[line][i] == SPACE)
+	if (cub->map.grid[line][i] == SPACE)
 	{
-		line = 0;
 		cub->map.grid[line][i] = OUTSIDE_SPACE;
-		while (cub->map.grid && cub->map.grid[line + 1]
-			&& cub->map.grid[line + 1][i] == SPACE)
-		{
-			cub->map.grid[line + 1][i] = OUTSIDE_SPACE;
-			line++;
-		}
-		i++;
+		if (cub->map.grid[line][i + 1] && cub->map.grid[line][i + 1] == SPACE)
+			ft_replace_first_and_last_line(cub, line, i + 1, first);
+		if (i > 0 && cub->map.grid[line][i - 1] && cub->map.grid[line][i - 1] == SPACE)
+			ft_replace_first_and_last_line(cub, line, i - 1, first);
+		if (first == true)
+			ft_replace_first_and_last_line(cub, line + 1, i, first);
+		else if (first == false && i > 0)
+			ft_replace_first_and_last_line(cub, line - 1, i, first);
 	}
+	ft_replace_first_and_last_line(cub, line, i + 1, first);
 }
 
 static void	ft_replace_sides(t_cub *cub, int line, int i, bool start)
@@ -61,9 +57,8 @@ void	ft_replace_outside_spaces(t_cub *cub)
 {
 	if (!cub)
 		return ;
-	ft_replace_sides(cub, 0, 0, true); //from left to right
+	ft_replace_sides(cub, 0, 0, true);
 	ft_replace_sides(cub, 0, cub->map_longest_line - 1, false);
-	//from right to left
-	ft_replace_first_line(cub);
-	//ft_replace_last_line(cub);
+	ft_replace_first_and_last_line(cub, 0, 0, true);
+	ft_replace_first_and_last_line(cub, cub-> map_line_counter - 1, 0, false);
 }
